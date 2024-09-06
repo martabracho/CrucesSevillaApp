391a641758b14dbbdbc3be8837fc32894035505a
martific <martabrachouned@gmail.com>
2024-09-06 13:12:09 +0200

    refactor: parametrizado de tipos de calles

--- app/src/main/java/com/example/crucessevillaapp/ui/screens/MapScreen.kt
+++ app/src/main/java/com/example/crucessevillaapp/ui/screens/MapScreen.kt
@@ -16,13 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
-import kotlinx.coroutines.CoroutineScope
+import com.example.crucessevillaapp.R
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.JsonObject
 import org.json.JSONArray
-import org.json.JSONObject
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
@@ -46,10 +43,19 @@ fun BodyContentMapScreen(navController: NavController, context: Context, direcci
     Configuration.getInstance().userAgentValue = "martific"
     //Initialize GeoPojnt
     var sevilleGeoPoint by remember { mutableStateOf(GeoPoint(37.38283000, -5.97317000))}
+    val context = LocalContext.current
 
     LaunchedEffect(direccion) {
         if (direccion != null) {
-            sevilleGeoPoint = fetchCoordinates(direccion)
+            val place1 = searchPlace1(direccion, context)
+            val place2 = searchPlace2(direccion, context)
+            sevilleGeoPoint = fetchCoordinates(place1)
+            if (place2!=null){
+                //Añadir calle a place2
+                val sevilleGeoPoint2 = fetchCoordinates(place2)
+                //comparar sevilleGeoPoint con sevilleGeoPoint2
+            }
+
             Log.d("Dirección", direccion)
             Log.d("Coordenadas", sevilleGeoPoint.toString())
         }
@@ -87,6 +93,55 @@ fun BodyContentMapScreen(navController: NavController, context: Context, direcci
 
 }
 
+/**
+ * Función que recibe una dirección y devuelve la primera parte de la misma
+ * y le añade la palabra "calle" si lo es
+ */
+fun searchPlace1(direccion: String, context: Context): String {
+    val auxDir = direccion.substringBefore("-")
+    var result = ""
+
+    if (auxDir.contains("PP.")){
+        result = auxDir.substringAfter("PP.")
+    } else {
+        result = auxDir
+    }
+
+    val street_type = context.resources.getStringArray(R.array.street_types)
+
+    if (street_type.any { keyword -> result.contains(keyword) }) {
+        return result.trim()
+    } else {
+        return "calle " + result.trim()
+    }
+
+}
+
+fun searchPlace2(direccion: String, context: Context): String {
+
+    if (direccion.contains("-")) {
+        val auxDir = direccion.substringAfter("-").substringBefore("-")
+        var result = auxDir
+        val street_type = context.resources.getStringArray(R.array.street_types)
+
+        if (auxDir.contains("PP.")){
+            result = auxDir.substringAfter("PP.")
+        } else {
+            result = auxDir
+        }
+
+        if (street_type.any { keyword -> result.contains(keyword) }) {
+            return result.trim()
+        } else {
+            return "calle " + result.trim()
+        }
+
+    } else {
+        return ""
+    }
+
+}
+
 suspend fun fetchCoordinates(direccion: String): GeoPoint {
     return withContext(Dispatchers.IO) {
         searchCoordinates(direccion)
@@ -94,7 +149,8 @@ suspend fun fetchCoordinates(direccion: String): GeoPoint {
 }
 
 suspend fun searchCoordinates(direccion: String): GeoPoint {
-    val url = "https://nominatim.openstreetmap.org/search?q=$direccion&format=json&limit=1"
+    val modifiedDir = direccion.replace(" ", "+")
+    val url = "https://nominatim.openstreetmap.org/search?street=$modifiedDir&viewbox=-6.0329183,37.4529579,-5.8191571,37.300203&bounded=1&format=json&limit=1"
     var lat = 0.0
     var lon = 0.0
 
